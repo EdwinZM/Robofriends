ba85d90321c822caecb080f6cdf5fa62b5d48e1d
EdwinZM <edwinzwa@hotmail.com>
2020-06-24 11:01:18 -0500

    updating

---
+++ src/components/Scroll.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './scroll.css';
+
+
+const Scroll = (props) => {
+    return (
+            <div style={{overflowY: 'scroll',  height: '500px'}}>
+                {props.children}
+            </div>
+        
+    )
+}
+
+export default Scroll;
